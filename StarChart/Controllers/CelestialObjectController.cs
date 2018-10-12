490592a2abab85e33d10606234464248db74bcc9
catchemash8 <jt19901411@gmail.com>
2018-10-12 15:49:22 +0800

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -74,7 +74,7 @@ namespace StarChart.Controllers
         [HttpPatch("{id}/{name}")]
         public IActionResult RenameObject(int id, string name)
         {
-            var existingObject = _context.CelestialObjects.Find();
+            var existingObject = _context.CelestialObjects.Find(id);
             if(existingObject == null)
                 return NotFound();
             existingObject.Name = name;
